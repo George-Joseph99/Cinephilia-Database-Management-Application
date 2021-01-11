91dc2cb0055e7e66fa5418fd4b5adbbb2293f2e0
Mark Nawar <62334815+Mark-Nawar@users.noreply.github.com>
2021-01-11 07:49:32 +0200

    directors

---
+++ Director_FilmMakers/dirsearch4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DBapplication.Director_FilmMakers
+{
+    public partial class dirsearch4 : Form
+    {
+        string username;
+        Controller controllerObj;
+        public dirsearch4(string user)
+        {
+            InitializeComponent();
+            username = user;
+            controllerObj = new Controller();
+        }
+
+        private void dirsearch4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable dt2 = controllerObj.myawards(username); 
+            dataGridView1.Refresh();
+        }
+    }
+}
