49d0fc85156f30701f8c0a563db30afa083e76df
Mark Nawar <62334815+Mark-Nawar@users.noreply.github.com>
2021-01-11 07:47:44 +0200

    The Admin files

---
+++ Admin/MAIN_ADMINS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DBapplication
+{
+    public partial class MAIN_ADMINS : Form
+    {
+        public MAIN_ADMINS()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
