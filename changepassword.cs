0ea5370f475dbde6449f08b77aa961e69eb29f32
Mark Nawar <62334815+Mark-Nawar@users.noreply.github.com>
2021-01-11 07:57:53 +0200

    Controller+DBmaanger

---
+++ changepassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DBapplication
+{
+    public partial class changepassword : Form
+    {
+        string username;
+        public changepassword(string user)
+        {
+            InitializeComponent();
+            username = user;
+        }
+
+        private void changepassword_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
