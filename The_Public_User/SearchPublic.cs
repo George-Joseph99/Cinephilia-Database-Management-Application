c3a480c67b99696f5cf1f927b639ed21c4e6e410
Mark Nawar <62334815+Mark-Nawar@users.noreply.github.com>
2021-01-11 07:56:23 +0200

    stars+movies+welcome

---
+++ The_Public_User/SearchPublic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DBapplication.The_Public_User
+{
+    public partial class SearchPublic : UserControl
+    {
+        public SearchPublic()
+        {
+            InitializeComponent();
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
